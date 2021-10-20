5c21348113b6e9600847259fdafc85efa4674232
Thiago Ausechi <thiago_ausechi@icloud.com>
2021-10-20 15:49:56 -0300

    Build #02.3.1
    - Added a Update API page
    
    - Xûr countdown working
    - Favicons added

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -1,5 +1,7 @@
 export default async function handler(req, res)
 {
     console.log(req.query);
+    console.log(process.env.NEXT_PUBLIC_UPDATE_AUTH_KEY);
+    console.log(req.query.auth === process.env.NEXT_PUBLIC_UPDATE_AUTH_KEY);
     res.status(200).json({});
 }
