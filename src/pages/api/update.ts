3c727d6a900e51db40c289710fa9a9ba2be1c646
Thiago Ausechi <thiago_ausechi@icloud.com>
2021-10-20 16:27:43 -0300

    Build #02.3.1
    - Added a Update API page
    
    - Xûr countdown working
    - Favicons added

--- src/pages/api/update.ts
+++ src/pages/api/update.ts
@@ -1,7 +1,22 @@
 export default async function handler(req, res)
 {
+    console.log("===================================================================");
+    const auth = req.query.auth;
+    const env_auth = process.env.NEXT_PUBLIC_UPDATE_AUTH_KEY;
+
     console.log(req.query);
-    console.log(process.env.NEXT_PUBLIC_UPDATE_AUTH_KEY);
-    console.log(req.query.auth === process.env.NEXT_PUBLIC_UPDATE_AUTH_KEY);
+    console.log("auth: " + auth);
+    console.log("update key: " + env_auth);
+    console.log("bungie key: " + process.env.NEXT_PUBLIC_BUNGIE_API_KEY);
+
+    if (auth === env_auth)
+    {
+        console.log("Do some code!");
+    }
+    else
+    {
+        console.error("Something went wrong")
+    }
+
     res.status(200).json({});
 }
